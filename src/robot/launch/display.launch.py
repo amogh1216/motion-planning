b9fcd016017477c3a7dc2732c5792598afe5ab4e
amogh1216 <amogh1216@gmail.com>
2025-06-25 06:49:51 +0000

    minor changes to cmake and launch files

--- src/robot/launch/display.launch.py
+++ src/robot/launch/display.launch.py
@@ -2,13 +2,14 @@ import os
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution, Command, FindExecutable
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
     # Set the package and URDF file name
-    pkg_name = 'robot'  # <-- change to your package name
+    pkg_name = 'robot'
     config_file = 'robot-config.rviz'
     rviz_config_path = PathJoinSubstitution([FindPackageShare(pkg_name), 'config', config_file])
 
@@ -17,8 +18,7 @@ def generate_launch_description():
     # Build the full path to the URDF file
     urdf_path = PathJoinSubstitution([FindPackageShare('robot'), 'urdf', urdf_file])
 
-    return LaunchDescription([
-        IncludeLaunchDescription(
+    launch_gazebo = IncludeLaunchDescription(
             PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
             launch_arguments={
                 'urdf_package': 'robot',
@@ -26,4 +26,7 @@ def generate_launch_description():
                 'rviz_config': rviz_config_path
             }.items()
         )
+
+    return LaunchDescription([
+        launch_gazebo
     ])
